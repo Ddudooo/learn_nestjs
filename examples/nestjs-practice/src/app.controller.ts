073d4958b7eff47f4a4da1d9cbab38cbb685b646
Ddudooo <igoa.work@gmail.com>
2021-12-03 00:41:15 +0900

    연습용 셋팅중

--- examples/nestjs-practice/src/app.controller.ts
+++ examples/nestjs-practice/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common'
+import { AppService } from './app.service'
+import { Request } from 'express'
 
 @Controller()
 export class AppController {
@@ -7,6 +8,18 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
+  }
+
+  @Get('/test')
+  getTest(@Req() req: Request): string {
+    console.log(req)
+    return this.constructor.name
+  }
+
+  @Post('/test')
+  postTest(@Body() body) {
+    console.log('바로 리퀘스트 바디 파싱', body)
+    return 'post test'
   }
 }
