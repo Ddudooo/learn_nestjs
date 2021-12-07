ae562b08dced3cc4ba48c6c022e28fae09161117
Ddudooo <igoa.work@gmail.com>
2021-12-07 23:29:27 +0900

    간단한 테스트 코드 추가
    서비스 테스트 추가
    jest matcher 찾다가 시간 다감

--- examples/nestjs-practice/src/cats/cats.service.spec.ts
+++ examples/nestjs-practice/src/cats/cats.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CatsService } from './cats.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { CatsRepository } from './cats.repository'
+import { plainToInstance } from 'class-transformer'
+import { Cat } from './entities/cat.entity'
+import { randomInt } from 'crypto'
 
 describe('CatsService', () => {
   let service: CatsService
+  let mockRepo: CatsRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
+      providers: [
+        CatsService,
+        { provide: getRepositoryToken(CatsRepository), useValue: {} },
+      ],
     }).compile()
 
     service = module.get<CatsService>(CatsService)
+    mockRepo = module.get(CatsRepository)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('모두 조회하면 저장된 값이 모두 찾아와야 한다.', async () => {
+    // given
+    const savedCats = []
+    for (let i = 0; i < 10; i++) {
+      const mockCat = plainToInstance(Cat, {
+        name: 'mock',
+        age: randomInt(1, 10),
+        species: 'mockCat',
+      })
+      savedCats.push(mockCat)
+    }
+    mockRepo.find = jest.fn().mockResolvedValue(savedCats)
+    // when
+    const response = await service.findAll()
+    // then
+    expect(response.length).toEqual(savedCats.length)
+    // ?? jest matcher 너무 구림.
+    expect(response).toEqual(expect.arrayContaining(savedCats))
   })
 })
