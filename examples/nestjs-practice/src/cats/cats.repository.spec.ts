c26189125eba0e0526c98accf997a3d09afc0053
Ddudooo <igoa.work@gmail.com>
2021-12-10 23:43:37 +0900

    간단한 테스트 코드 추가

--- examples/nestjs-practice/src/cats/cats.repository.spec.ts
+++ examples/nestjs-practice/src/cats/cats.repository.spec.ts
@@ -3,6 +3,7 @@ import { Connection } from 'typeorm'
 import { Cat } from './entities/cat.entity'
 import { Test } from '@nestjs/testing'
 import { CatsRepository } from './cats.repository'
+import { plainToInstance } from 'class-transformer'
 
 describe('리포지토리 유닛 테스트', () => {
   let connection: Connection
@@ -36,5 +37,23 @@ describe('리포지토리 유닛 테스트', () => {
 
   it('테스트 구성 확인', async () => {
     expect(repository).toBeDefined()
+  })
+
+  it('정상적으로 저장되어야 한다.', async () => {
+    // given
+    const catDto: Cat = plainToInstance(Cat, {
+      name: 'TEST_CAT',
+      age: 10,
+      species: 'TEST',
+    })
+
+    // when
+    const saved = await repository.save(catDto, { reload: true })
+
+    // then
+    expect(saved.id).toBeDefined()
+    expect(saved.name).toEqual(catDto.name)
+    expect(saved.age).toEqual(catDto.age)
+    expect(saved.species).toEqual(catDto.species)
   })
 })
