3001711e47b91bd65920567fb75168b76243f061
Ddudooo <igoa.work@gmail.com>
2021-12-18 21:54:55 +0900

    access-control 예제 준비

---
+++ examples/access-control/src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+const TypeOrmConfig = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrationsTableName: 'migration',
+  migrations: ['src/db/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+  logging: 'all',
+  synchronize: true,
+};
+
+export default TypeOrmConfig;
