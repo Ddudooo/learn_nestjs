c36a7679fee43f47ceb9872c392fbc6c38668d87
Ddudooo <igoa.work@gmail.com>
2021-12-27 21:40:14 +0900

    요상한곳에서 요상한 오류나네 ??

--- examples/access-control/src/app.module.ts
+++ examples/access-control/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { RolesModule } from './roles/roles.module'
+import { AccessControlModule, RolesBuilder } from 'nest-access-control'
+import { RolesService } from './roles/roles.service'
 
 @Module({
-  imports: [UsersModule, RolesModule], // AccessControlModule.forAsyncRoles(roles) // 음... 안되네
+  imports: [
+    UsersModule,
+    RolesModule,
+    AccessControlModule.forRootAsync({
+      imports: [RolesModule],
+      inject: [RolesService],
+      useFactory: async (roleService: RolesService): Promise<RolesBuilder> => {
+        return new RolesBuilder(await roleService.getRoles())
+      },
+    }),
+  ], // AccessControlModule.forAsyncRoles(roles) // 음... 안되네
   controllers: [AppController],
   providers: [AppService],
 })
